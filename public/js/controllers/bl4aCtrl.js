060bc70fa7a0252a208ae7a8e7e60d9e0967bb5d
leandro2m <lmmoraes@gmail.com>
2017-05-13 18:51:45 -0300

    hyperlink fix

---
+++ public/js/controllers/bl4aCtrl.js
@@ -0,0 +1,25 @@
+angular.module('App').controller('bl4aCtrl', function($scope,$resource){
+   
+     $scope.exportData = function () {
+        var blob = new Blob([document.getElementById('exportable').innerHTML], {
+            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
+        });
+        saveAs(blob, "Relatorio_bloco4a.xls");
+    };
+
+	$scope.items = [];
+	
+	var apiData = $resource('/api/data/1/UCSReserv1/bl4a');
+	function buscaData() {
+		apiData.query(
+			function(items) {		
+				$scope.items = items;
+			},
+			function(erro) {
+				console.log(erro);
+				$scope.mensagem = {texto: 'Não foi possível obter os dados'};
+			}
+		);
+	}
+buscaData();
+})
